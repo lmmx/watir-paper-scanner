0d356708afa2eed632adc491a40c4ab73864fa27
lmmx <maddo16@hotmail.co.uk>
2013-10-03 22:15:12 +0100

    Highlighting the first DOI by (common?) class
    This is what needs pulling out into a file

--- doiconverter.rb
+++ doiconverter.rb
@@ -3,15 +3,18 @@
 require "watir-webdriver"
 browser = Watir::Browser.new :chrome
 
-sheetpub = "https://docs.google.com/spreadsheet/pub?key=0Aj697J8sF_ekdHM4NVBRZWV0eXFERGxrWEdzSlRReUE&single=true&gid=5&output=html"
+sheetpub = "http://goo.gl/2T60tU"
 doipre = "http://dx.doi.org/"
 doisuff = ""
 
 browser.goto sheetpub
-#get first doi from spreadsheet
-puts browser.url
-browser.goto doipre + doisuff #need to concatenate DOI here
+#highlight first doi from spreadsheet
+browser.td(:class => "s24").flash
 
-#need to either add in-browser or save as .txt .csv or .xls file
+#concatenate with doipre, output the url it leads to with:
+browser.goto doipre + doisuff #need to concatenate DOI here
+#puts browser.url
+
+#need to either add this output in-browser (i.e. in a spreadsheet cell) or save as .txt .csv or .xls file
 
 browser.close        # call it a day
