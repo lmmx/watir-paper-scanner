4bad88d78adff393dcc37906e96a8f36c56c8017
lmmx <maddo16@hotmail.co.uk>
2013-10-24 21:43:47 +0100

    Brought in roo library, stated relevant URL variables

--- bookworm.rb
+++ bookworm.rb
@@ -1,19 +1,33 @@
 #!/usr/bin/env ruby
 
 require "watir-webdriver"
+require "roo"
+
+#set up some info
+doipre = "http://dx.doi.org/"
+sheetkey="0Aj697J8sF_ekdHM4NVBRZWV0eXFERGxrWEdzSlRReUE"
+sheetfull="https://docs.google.com/spreadsheet/ccc?key=" + sheetkey
+sheetresults = "#gid=5"
+sheetprocessed = "#gid=4"
+results = sheetfull + sheetresults
+nameslist = sheetfull + sheetprocessed
+
+oo = Google.new('"'+sheetkey+'"')
+#now what
+
 browser = Watir::Browser.new :chrome
 
-#before the loop
-doipre = "http://dx.doi.org/"
-sheetpub = "https://docs.google.com/spreadsheet/pub?key=0Aj697J8sF_ekdHM4NVBRZWV0eXFERGxrWEdzSlRReUE&single=true&gid=5&output=html"
-
 #enter the loop
-browser.goto doipre #+ DOI-from-sheetpub (need to concatenate DOI here, variable should be in a loop going down DOI list)
+browser.goto doipre #+ DOI-from-sheet (need to concatenate DOI here, variable should be in a loop going down DOI list)
 #if browser.text.include? '*'
-#   puts browser.a(:text => "still need some way to find the text with asterisk").flash
+#   puts browser.a(:text => "still need to get regex to find text with asterisk").flash
 #else
 #   hmm_not_sure
 
 #leave watir running while all the papers get scanned (or open new tabs and run parallel...?)
 
 browser.close        # call it a day
+
+browser.goto sheetpub
+
+#need to either add the output in-browser (i.e. in a spreadsheet cell) or save as .txt .csv or .xls file
