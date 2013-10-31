6ca1987797c2f138b589c70144c41b92cdba418a
lmmx <maddo16@hotmail.co.uk>
2013-10-31 16:29:32 +0000

    Goes to DOI for line 2 and stores the URL as a matter of course in column P
    Trying to get this to iterate down the entire sheet but not much luck.
    
    After this will need to mine each page when the browser navigates to it for the asterisked author.

--- bookworm.rb
+++ bookworm.rb
@@ -5,13 +5,10 @@ require "google_drive"
 require "roo"
 
 #set up some info
-doipre = "http://dx.doi.org/"
 sheetkey="0Aj697J8sF_ekdHM4NVBRZWV0eXFERGxrWEdzSlRReUE"
-sheetfull="https://docs.google.com/spreadsheet/ccc?key=" + sheetkey
-sheetresults = "#gid=5"
-sheetprocessed = "#gid=4"
-results = sheetfull + sheetresults
-nameslist = sheetfull + sheetprocessed
+sheetfull="https://docs.google.com/spreadsheet/ccc?key=" + sheetkey   #use this to see the actual sheet
+sheetresults = "#gid=5"                 #append this to the URL for the sheet this program will use (pubmed_result)
+results = sheetfull + sheetresults      #hey look I did it for you...
 
 puts "Please enter your Gmail address"
 GOOGLE_MAIL = gets
@@ -20,7 +17,6 @@ GOOGLE_PASSWORD = gets
 #GOOGLE_MAIL = "For ease you can just set it here instead of being questioned"
 #GOOGLE_PASSWORD = "^Ditto^"
 
-
 browser = Watir::Browser.new :chrome
 
 # This passes the login credentials to Roo without requiring every user change system environment variables
@@ -28,10 +24,18 @@ oo = Roo::Google.new(sheetkey, user: GOOGLE_MAIL, password: GOOGLE_PASSWORD) #Lo
 oo.default_sheet = "pubmed_result"
 
 browser.goto oo.cell(2,'P') #grabs the DOI and navigates to journal webpage (only where a D.O.I. is available)
+oo.set(line,'T',browser.url)
 
 #the following doesn't seem to work as desired but in theory it names each cell when on that row, simplifies things:
 
-1.upto(4) do |line|       #change "(4)" for "(oo.last_row)" when you've verified it works and run headless not Chrome
+5.upto(8) do |line|       #change "(4)" for "(oo.last_row)" when you've verified it works and run headless not Chrome
+  realurl        = oo.cell(line,'T')
+  if realurl
+    browser.goto oo.cell(line,'P')
+    oo.set(line,'T',browser.url)
+  end
+end    #why does this just "puts" out the first line number rather than change anything in the spreadsheet??
+=begin        excuse me while I comment out the irrelevant bits for the time being
   lastauth       = oo.cell(line,'A')
   1authsurname   = oo.cell(line,'B')
   unique1authsur = oo.cell(line,'C')
@@ -48,12 +52,14 @@ browser.goto oo.cell(2,'P') #grabs the DOI and navigates to journal webpage (onl
   doiuri         = oo.cell(line,'P')
   doixml         = oo.cell(line,'Q')
   pmceutilsxml   = oo.cell(line,'R')
+  realurl        = oo.cell(line,'T')
   pmcid          = oo.cell(line,'U')
   pubmeduri      = oo.cell(line,'V')
   identifiers    = oo.cell(line,'W')
   pmid           = oo.cell(line,'X')
   properties     = oo.cell(line,'Y')
   1authfull      = oo.cell(line,'Z')
+=end
   
 #Although this bit doesn't work, I don't actually want the array to be spat out, I just want
 #the DOIs one at a time for my browser to navigate to. I don't have a need for a massive list of URLs
@@ -74,4 +80,4 @@ browser.goto oo.cell(2,'P') #grabs the DOI and navigates to journal webpage (onl
 
 browser.close        # call it a day
 
-#do I need to end this code with an "end" ? Who can say...
+#do I need to end this code with an "end" ?
