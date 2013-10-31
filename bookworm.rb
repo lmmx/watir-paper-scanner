023b623da081758d14f8239168f76a0004d2fc53
lmmx <maddo16@hotmail.co.uk>
2013-10-31 13:52:13 +0000

    Describing the sheet
    Assigned columns to variables

--- bookworm.rb
+++ bookworm.rb
@@ -19,7 +19,31 @@ puts "Please enter your password"
 GOOGLE_PASSWORD = gets
 
 # This passes the login credentials to Roo without requiring every user change system environment variables
-oo = Roo::Google.new(sheetkey, user: GOOGLE_MAIL, password: GOOGLE_PASSWORD)
+oo = Roo::Google.new(sheetkey, user: GOOGLE_MAIL, password: GOOGLE_PASSWORD) #Woop de doo you're in
+oo.default_sheet = "pubmed_result"
+1.upto(oo.last_row) do |line|
+  lastauth       = oo.cell(line,'A')
+  1authsurname   = oo.cell(line,'B')
+  unique1authsur = oo.cell(line,'C')
+  correspauth    = oo.cell(line,'D')
+  hyplink        = oo.cell(line,'E')
+  title          = oo.cell(line,'F')
+  pmurlfield     = oo.cell(line,'G')
+  pmid           = oo.cell(line,'H')
+  journal        = oo.cell(line,'I')
+  authors        = oo.cell(line,'J')
+  details        = oo.cell(line,'K')
+  nodoi          = oo.cell(line,'L')
+  doi            = oo.cell(line,'O')
+  doiuri         = oo.cell(line,'P')
+  doixml         = oo.cell(line,'Q')
+  pmceutilsxml   = oo.cell(line,'R')
+  pmcid          = oo.cell(line,'U')
+  pubmeduri      = oo.cell(line,'V')
+  identifiers    = oo.cell(line,'W')
+  pmid           = oo.cell(line,'X')
+  properties     = oo.cell(line,'Y')
+  1authfull      = oo.cell(line,'Z')
 
 #now what
 
