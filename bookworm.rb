c11216fbe34388199ec1c941dac470f5f709c235
lmmx <maddo16@hotmail.co.uk>
2013-10-31 15:34:36 +0000

    Navigating to papers from DOI
    Example of 1 cell being navigated to, next a regular expression and Watir function are needed to pull the corresponding author out, then it's just a simple matter of repetition down to the last row.

--- bookworm.rb
+++ bookworm.rb
@@ -17,11 +17,21 @@ puts "Please enter your Gmail address"
 GOOGLE_MAIL = gets
 puts "Please enter your password"
 GOOGLE_PASSWORD = gets
+#GOOGLE_MAIL = "For ease you can just set it here instead of being questioned"
+#GOOGLE_PASSWORD = "^Ditto^"
+
+
+browser = Watir::Browser.new :chrome
 
 # This passes the login credentials to Roo without requiring every user change system environment variables
-oo = Roo::Google.new(sheetkey, user: GOOGLE_MAIL, password: GOOGLE_PASSWORD) #Woop de doo you're in
+oo = Roo::Google.new(sheetkey, user: GOOGLE_MAIL, password: GOOGLE_PASSWORD) #Loading spreadsheet :-)
 oo.default_sheet = "pubmed_result"
-1.upto(oo.last_row) do |line|
+
+browser.goto oo.cell(2,'P') #grabs the DOI and navigates to journal webpage (only where a D.O.I. is available)
+
+#the following doesn't seem to work as desired but in theory it names each cell when on that row, simplifies things:
+
+1.upto(4) do |line|       #change "(4)" for "(oo.last_row)" when you've verified it works and run headless not Chrome
   lastauth       = oo.cell(line,'A')
   1authsurname   = oo.cell(line,'B')
   unique1authsur = oo.cell(line,'C')
@@ -44,22 +54,24 @@ oo.default_sheet = "pubmed_result"
   pmid           = oo.cell(line,'X')
   properties     = oo.cell(line,'Y')
   1authfull      = oo.cell(line,'Z')
+  
+#Although this bit doesn't work, I don't actually want the array to be spat out, I just want
+#the DOIs one at a time for my browser to navigate to. I don't have a need for a massive list of URLs
+#but it can stay while I understand how to use this thing
 
-#now what
+#this bit doesn't work  if pubmeduri
+#this bit doesn't work    puts '#{pubmeduri}\t#{title}'
 
-browser = Watir::Browser.new :chrome
-
-#enter the loop
-browser.goto doipre #+ DOI-from-sheet (need to concatenate DOI here, variable should be in a loop going down DOI list)
+#idea for a RegExp to get titles:
 #if browser.text.include? '*'
 #   puts browser.a(:text => "still need to get regex to find text with asterisk").flash
 #else
 #   hmm_not_sure
 
-#leave watir running while all the papers get scanned (or open new tabs and run parallel...?)
+#leave watir running while all the papers get scanned
+#(or open new tabs and run parallel...?)
+#(or better yet, run a headless browser - might not see errors though if they are made)
 
 browser.close        # call it a day
 
-browser.goto sheetpub
-
-#need to either add the output in-browser (i.e. in a spreadsheet cell) or save as .txt .csv or .xls file
+#do I need to end this code with an "end" ? Who can say...
